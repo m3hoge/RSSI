739e6600b8a5d1a7a1d9993f54376c20ef72a250
m3hoge <m3hoge@gmail.com>
2025-03-12 13:25:53 +0900

    Create install_google_chrome.sh

---
+++ install_google_chrome.sh
@@ -0,0 +1,8 @@
+# パッケージの取得
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# インストール
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+
+# 残骸を削除
+rm ./google-chrome-stable_current_amd64.deb 
